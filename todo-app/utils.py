a353411998ccfc176d3b62665cb21e8f3438f556
Simeon Ezekiel <imonight1@gmail.com>
2025-12-05 10:34:55 +0100

    decorators

--- todo-app/utils.py
+++ todo-app/utils.py
@@ -3,9 +3,9 @@ from functools import wraps
 def log_action(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
-        print(f"[log] is runnung {func.__name__()}")
+        print(f"[log] is running {func.__name__}")
         result = func(*args, **kwargs)
-        print(f"[log] is finsished {func.__name__}()")
+        print(f"[log] is finished {func.__name__}")
         return result
     return wrapper
 
@@ -15,7 +15,8 @@ def validate_input(func):
     def wrapper(tasks, *args, **kwargs):
         if not tasks.strip():
             print("tasks cannot be empty")
-            return func(tasks, *args, **kwargs)
+            return
+        return func(tasks, *args, **kwargs)
         
     return wrapper
 
